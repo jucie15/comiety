b76026f963f7f8f054b0d619c0f05d6d50c75df9
gustos <jucie15@nate.com>
2017-01-24 16:30:02 +0900

    model design(관계 추가)

--- comiety/dongzip/admin.py
+++ comiety/dongzip/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import User, School, Society, Event
+from .models import Profile, School, Society, Event
 
-class  UserAdmin(admin.ModelAdmin):
+class  ProfileAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'email')
-    model = User
+    #list_display = ('name', 'email')
+    model = Profile
 
 class SchoolAdmin(admin.ModelAdmin):
     model = School
@@ -15,7 +15,7 @@ class SocietyAdmin(admin.ModelAdmin):
 class EventAdmin(admin.ModelAdmin):
     model = Event
 
-admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(School, SchoolAdmin)
 admin.site.register(Society, SocietyAdmin)
 admin.site.register(Event, EventAdmin)
--- comiety/dongzip/models.py
+++ comiety/dongzip/models.py
@@ -1,18 +1,11 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 # Create your models here.
 
 
 
-class User(models.Model):
-    name = models.CharField(max_length = 128, blank = False, null = False) # 유저 이름
-    nickname = models.CharField(max_length = 128, blank = False, null = False) # 닉네임
-    #favorite = 태그로 ㄱ?
-    tel_number = models.CharField(max_length = 32, blank = False, null = False) # 전화번호
-    email = models.EmailField(max_length = 128, blank = False, null = False) # 이메일
 
-    def __str__(self):
-        return self.name
 
 class School(models.Model):
     name = models.CharField(max_length = 128, blank = False, null = False) # 학교 이름
@@ -24,12 +17,26 @@ class School(models.Model):
     def __str__(self):
         return self.name
 
+class Profile(models.Model):
+    # user = models.OneToOneField(User)
+    # user = models.OneToOneField('auth.User')
+    school = models.ForeignKey(School) # School Table과 1:n 관계 형성
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)  # 'auth.User'
+    nickname = models.CharField(max_length = 128, blank = False, null = False) # 닉네임
+    #favorite = 태그로 ㄱ?
+    tel_number = models.CharField(max_length = 32, blank = False, null = False) # 전화번호
+
+    def __str__(self):
+        return self.user.username
+
 class Society(models.Model):
+    school = models.ForeignKey(School) # School Table과 1:n 관계 형성
     name = models.CharField(max_length = 128, blank = False, null = False) # 동아리 이름
     main_tel_number = models.CharField(max_length = 32, blank = False, null = False) # 대표 전화번호
     member_number = models.IntegerField(default = 0, blank = False, null = False) # 멤버 수
     address = models.CharField(max_length = 256, blank = False, null = False) # 동아리 주소
     description = models.TextField(max_length = 512, blank = True, null = True) # 동아리 소개
+    users = models.ManyToManyField(Profile) # USER TABLE과 다대다 관계 형성
     #category  = models.CharField # 관심사 및 카테고리를 따로 빼야하나?
 
     def __str__(self):
@@ -42,8 +49,12 @@ class Event(models.Model):
     updated_at = models.DateTimeField(auto_now_add = True) # 행사 일정 수정 시간
     description = models.TextField(max_length = 512, blank = True, null = True) # 행사 소개
 
-    # home_team = models.ForeignKey(Society)
-    # away_team = models.ForeignKey(Society)
+    home_team = models.ForeignKey(Society, related_name='home_event_set') # 주최 팀
+    # for event in society.home_event_set.all()으로 접근 가능
+
+    # away_team_set = models.ManyToManyField(Society) # 어웨이 팀이 여려 팀일 경우
+    away_team = models.ForeignKey(Society, related_name='away_event_set') # 상대 팀
+    # society.away_event_set.all()으로 접근 가능
 
     def __str__(self):
         return self.title
