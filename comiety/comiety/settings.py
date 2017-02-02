48621dabed9f80456c455d60c67b1a9a6471c4d0
YangGa Eun <joy24@YangGaui-MacBook-Pro.local>
2017-02-02 14:25:57 +0900

    template&image

--- comiety/comiety/settings.py
+++ comiety/comiety/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'comiety.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'comiety', 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
