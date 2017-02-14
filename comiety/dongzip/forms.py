ffc856e6c90d183181124cfd16e3b6215f98c182
gustos <jucie15@nate.com>
2017-02-14 19:45:16 +0900

    test

--- comiety/dongzip/forms.py
+++ comiety/dongzip/forms.py
@@ -5,4 +5,6 @@ class SocietyForm(ModelForm):
     class Meta:
         model = Society
         #fields = '__all__'
-        fields = ['school', 'name', 'main_tel_number', 'address', 'description', 'users']
+        fields = [
+            'school', 'name', 'main_tel_number','description', 'background_image', 'logo_image'
+            ]
--- dongzip/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Society, Profile
-
-class SocietyForm(ModelForm):
-    class Meta:
-        model = Society
-        #fields = '__all__'
-        fields = ['school', 'name', 'main_tel_number', 'address', 'description', 'users']
