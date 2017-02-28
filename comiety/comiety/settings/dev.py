4c86e008371d40da03a7daedb05f3caf09d60081
gustos <jucie15@nate.com>
2017-02-28 22:29:30 +0900

    INTERNAL_IPS = [127.0.0.1] 추가

--- comiety/comiety/settings/dev.py
+++ comiety/comiety/settings/dev.py
@@ -1,3 +1,4 @@
 from .common import *
 
 
+INTERNAL_IPS = ['127.0.0.1']
--- comiety/comiety/settings/prod.py
+++ comiety/comiety/settings/prod.py
@@ -5,6 +5,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+
 STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
